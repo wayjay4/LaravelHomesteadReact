525d6b87f1303b548c481f32da911bd0ba1d0350
Waylon Dixon <wdixon@waylondixon.com>
2017-09-12 03:37:38 +0000

    updated welcome header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My Laravel: ReactJS
+                    My Laravel: ReactJS <br /> now with GitHub
                 </div>
 
                 <div class="links">
